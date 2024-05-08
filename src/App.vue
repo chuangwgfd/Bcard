8685fdda9eba1e6b46fcfb3cacf571474b7b0957
William.chuang <william.chuang@collaborate.tw>
2024-05-08 17:50:47 +0800

    temp

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,42 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <header class="header"></header>
+    <section class="center">
+      <nav class="menu">
+        <ul>
+          <li>所有看板</li>
+        </ul>
+      </nav>
+      <main>
+        <h1>都市傳說</h1>
+      </main>
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="sass">
+  .header
+    height: 48px
+    background-color: #006aa6
+  .center
+    display: flex
+    width: 1260px
+    margin: 0 auto
+    padding: 0 135px
+  main
+    width: 752px
+    margin: 20px 12px 0
+    padding: 20px 60px 0
+    background-color: #fff
 </style>
